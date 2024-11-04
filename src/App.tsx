a1deab46527de51e4e43dd7470390eb511a2b05a
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2024-11-04 16:16:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blue-600">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
