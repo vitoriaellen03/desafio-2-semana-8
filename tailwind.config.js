7a823935784be50e1eab0bbdaa3cd1a42b8a5cb0
Eduardo Miguel <eduardomiguel.r.m@outlook.com>
2024-11-05 13:59:43 -0300

    feat: adding footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E293B",
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+      spacing: {
+        hundrend: "100px",
+        sixty: "60px"
+      },
+      gradientColorStops: {
+        "gradient-purple": "#FC04BA",
+        "gradient-yellow": "#FFC702",
+      },
+    },
   },
   plugins: [],
-};
+}
