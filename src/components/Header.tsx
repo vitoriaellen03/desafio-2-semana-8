fc45194f9a8fc4de5d896700c1de0a9ccdd796f1
vitoriaellen03 <contato.vitoriaellen03@gmail.com>
2024-11-05 12:48:53 -0300

    Adicionando tag html header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header class="c-header" id="c-header">
+      <nav class="navbar columns">
+        <div class="logo column">
+          <a href="/">
+            <img src="/assets/images/logo.svg" alt="Logo"/>
+              <h2 class="title">Project Manager</h2>
+          </a>
+        </div>
+
+        <div class="column menu">
+          <ul class="nav-menu">
+          </ul>
+          <div class="hamburger default"></div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
