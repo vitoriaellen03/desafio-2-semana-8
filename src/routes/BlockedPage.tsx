b6934c87fa8ac8773f3f63ed47b5bb18e69f7d4f
Eduardo Miguel <eduardomiguel.r.m@outlook.com>
2024-11-11 17:13:37 -0300

    fix: fix some routes

--- src/routes/BlockedPage.tsx
+++ src/routes/BlockedPage.tsx
@@ -15,7 +15,7 @@ const BlockedPage = () => {
           <div>
             <h1 className="text-8xl pb-4">403</h1>
             <p className="text-justify text-lg">
-            Nothing to see here... yet! To see this content, you need to <a className="text-decoration: underline" href="/">log in.</a> 
+            Nothing to see here... yet! To see this content, you need to <a className="text-decoration: underline" href="/login">log in.</a> 
             </p>
           </div>
         </section>
