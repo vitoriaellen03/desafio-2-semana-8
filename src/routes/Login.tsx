e7ab6c270a76e433434018fde3c6d19ccd4d973a
Maria <marycarol.00@gmail.com>
2024-11-11 12:32:28 -0300

    fix: correcting the user object

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,9 +5,40 @@ import { Link } from "react-router-dom";
 import { SignInButton } from "@clerk/clerk-react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { validateEmail, validatePassword } from "../types/validate";
 
 const Login = () => {
+
+  const validateEmail = () => {
+
+    const email = document.getElementById("email") as HTMLInputElement;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+
+    if (!emailRegex.test(email.value)) {
+      email.placeholder = "Invalid email";
+      email.classList.add("erro");
+    } else {
+      email.placeholder = "Enter your email";
+      email.classList.remove("erro");
+    }
+  } 
+
+  const validatePassword = () => {
+    
+    const password = document.getElementById("password") as HTMLInputElement;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/i;
+    const span = document.getElementById("spanError") as HTMLSpanElement;
+
+    if (!passwordRegex.test(password.value)) {
+      span.classList.add("erro-message")
+      span.classList.remove("hidden");
+      password.classList.add("erro");
+    } else {
+      password.classList.remove("erro");
+      span.classList.remove("erro-message");
+      span.classList.add("hidden");
+    }
+  }
+  
 
   return (
     <>
